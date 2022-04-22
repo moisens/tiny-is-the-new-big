a58351d3ef1fe0508ce162e61fdc167d37f378a2
moise <mose.ns@hotmail.com>
2022-04-22 17:47:04 +0200

    dropdown functionality + type usestate

--- frontend/src/types/Interface.ts
+++ frontend/src/types/Interface.ts
@@ -1,5 +1,6 @@
 export interface ILinkProps {
   linksData: { id: string; text: string; url: string }[];
+  setIsDropdown?: React.Dispatch<React.SetStateAction<boolean>>
   //linksData: LinkType[]; => Can also write like this!!!
 }
 
