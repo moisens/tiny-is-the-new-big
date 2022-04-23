95df44a747e10a3f36f1dfd4dbf66e545b005b46
moise <mose.ns@hotmail.com>
2022-04-23 23:55:51 +0200

    nav sidebar component css

---
+++ frontend/src/components/nav/Sidebarav.tsx
@@ -0,0 +1,18 @@
+import "./nav.scss"
+
+
+
+const Sidebarav = () => {
+  return (
+    <section className="modal__sidebar active__modal">
+      <aside className="sidebar__container">
+        <ul className="sidebar__ul">
+          <li className="sidebar__li">Home</li>
+          <li className="sidebar__li">Home</li>
+        </ul>
+      </aside>
+    </section>
+  )
+}
+
+export default Sidebarav;
