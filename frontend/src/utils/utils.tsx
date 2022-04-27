2506b98b79c52f241003a95af3e11b1b9ca5d309
moise <mose.ns@hotmail.com>
2022-04-27 03:40:16 +0200

    added founders data in utils mapped through founders data in about componrnt

---
+++ frontend/src/utils/utils.tsx
@@ -0,0 +1,100 @@
+import { AiFillStar } from "react-icons/ai";
+
+
+export const linkNav = [
+  {
+    id: "c316e59e-b909",
+    text: "Buy",
+    url: "/buy"
+  },
+  {
+    id: "c316e79c-b909",
+    text: "Rent",
+    url: "/rent"
+  },
+  {
+    id: "c316e9fe-b909",
+    text: "Locations",
+    url: "/location"
+  }
+];
+
+
+export const dropdownLinks = [
+  {
+    id: "b7205456-c196",
+    text: "About",
+    url: "#about"
+  },
+  {
+    id: "b7205ae6-c196",
+    text: "Services",
+    url: "#services"
+  },
+  {
+    id: "b7205d98-c196",
+    text: "Contact",
+    url: "#contact"
+  },
+];
+
+export const founders = [
+  {
+    id: "457df902-c5c4",
+    name: "Kyra Nerys",
+    title: "Co-founder",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1649899897/tiny%20is%20the%20new%20big/christopher-campbell-rDEOVtE7vOs-unsplash_vnbxj5.jpg",
+    icons: [
+      {
+        id: "632f341c-c5c8", 
+        star: <AiFillStar />
+      },
+      {
+        id: "632f3764-c5c8", 
+        star: <AiFillStar />
+      },
+      {
+        id: "632f3cd2-c5c8", 
+        star: <AiFillStar />
+      },
+      {
+        id: "632f3ed0-c5c8", 
+        star: <AiFillStar />
+      },
+      {
+        id: "632f40a6-c5c8", 
+        star: <AiFillStar />
+      },
+    ],
+    contact: "Contact" 
+  },
+  {
+    id: "457dfb96-c5c4",
+    name: "Ezrin Dax",
+    title: "Co-founder",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1649902871/tiny%20is%20the%20new%20big/jake-nackos-IF9TK5Uy-KI-unsplash_cjx1sp.jpg",
+    icons: [
+      {
+        id: "632f4268-c5c8", 
+        star: <AiFillStar />
+      },
+      {
+        id: "632f443e-c5c8", 
+        star: <AiFillStar />
+      },
+      {
+        id: "632f457e-c5c8", 
+        star: <AiFillStar />
+      },
+      {
+        id: "632f4920-c5c8", 
+        star: <AiFillStar />
+      },
+      {
+        id: "632f4a88-c5c8", 
+        star: <AiFillStar />
+      },
+    ],
+    contact: "Contact" 
+  },
+];
