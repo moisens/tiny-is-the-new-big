f499dd120f93e4e9a6289e16bd6df16cacd1dd9c
moise <mose.ns@hotmail.com>
2022-05-01 04:13:24 +0200

    slider component

--- frontend/src/utils/utils.tsx
+++ frontend/src/utils/utils.tsx
@@ -98,3 +98,146 @@ export const founders = [
     contact: "Contact" 
   },
 ];
+
+export const sliderPeople = [
+  {
+    id: "07b3590e-c8ef",
+    name: "Jackeline Snow",
+    text: "Broker ipsum dolor sit amet, consectetuer noted architect elit. Modern interior commodo ligula eget dolor. Cum sociis panoramic views penatibus et magnis dis downtown loft.",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1651369450/tiny%20is%20the%20new%20big/aleksandr-minakov-xDyqR14KyAs-unsplash_hcca4m.jpg",
+    icons: [
+      {
+        id: "07b35ba2-c8ef", 
+        star: <AiFillStar />
+      },
+      {
+        id: "07b35cec-c8ef", 
+        star: <AiFillStar />
+      },
+      {
+        id: "07b35e2c-c8ef", 
+        star: <AiFillStar />
+      },
+      {
+        id: "07b35f58-c8ef", 
+        star: <AiFillStar />
+      },
+      {
+        id: "07b3608e-c8ef", 
+        star: <AiFillStar />
+      },
+    ]
+  },
+  {
+    id: "07b364b2-c8ef",
+    name: "Natalie Dearbon",
+    text: "Broker ipsum dolor sit amet, consectetuer noted architect elit. Modern interior commodo ligula eget dolor. Cum sociis panoramic views penatibus et magnis dis downtown loft.",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1651369450/tiny%20is%20the%20new%20big/houcine-ncib-B4TjXnI0Y2c-unsplash_tqhxul.jpg",
+    icons: [
+      {
+        id: "07b365f2-c8ef", 
+        star: <AiFillStar />
+      },
+      {
+        id: "07b3670a-c8ef", 
+        star: <AiFillStar />
+      },
+      {
+        id: "07b36854-c8ef", 
+        star: <AiFillStar />
+      },
+      {
+        id: "07b36976-c8ef", 
+        star: <AiFillStar />
+      },
+      {
+        id: "07b36a8e-c8ef", 
+        star: <AiFillStar />
+      },
+    ]
+  },
+  {
+    id: "ac7747ce-c8f0",
+    name: "Patrick Mitchel",
+    text: "Broker ipsum dolor sit amet, consectetuer noted architect elit. Modern interior commodo ligula eget dolor. Cum sociis panoramic views penatibus et magnis dis downtown loft.",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1651369450/tiny%20is%20the%20new%20big/ransford-quaye-DzAFv1iVMGg-unsplash_q7ainw.jpg",
+    icons: [
+      {
+        id: "ac774986-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac774c4c-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac774d46-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac774e2c-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac774f4e-c8f0", 
+        star: <AiFillStar />
+      },
+    ]
+  },
+  {
+    id: "ac77502a-c8f0",
+    name: "Haskell Gilligan",
+    text: "Broker ipsum dolor sit amet, consectetuer noted architect elit. Modern interior commodo ligula eget dolor. Cum sociis panoramic views penatibus et magnis dis downtown loft.",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1651369450/tiny%20is%20the%20new%20big/ian-dooley-d1UPkiFd04A-unsplash_cxna7c.jpg",
+    icons: [
+      {
+        id: "ac7750fc-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac7751d8-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac7752aa-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac775e44-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac775f3e-c8f0", 
+        star: <AiFillStar />
+      },
+    ]
+  },
+  {
+    id: "ac776010-c8f0",
+    name: "Herbert Holmes",
+    text: "Broker ipsum dolor sit amet, consectetuer noted architect elit. Modern interior commodo ligula eget dolor. Cum sociis panoramic views penatibus et magnis dis downtown loft.",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1651369450/tiny%20is%20the%20new%20big/joseph-gonzalez-iFgRcqHznqg-unsplash_aau0ly.jpg",
+    icons: [
+      {
+        id: "ac7760f6-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac7761e6-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac776326-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac776402-c8f0", 
+        star: <AiFillStar />
+      },
+      {
+        id: "ac7766fa-c8f0", 
+        star: <AiFillStar />
+      },
+    ]
+  },
+];
