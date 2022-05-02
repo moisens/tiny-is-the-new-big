75b75589c82a947d417c869bf10305f4a07b705a
moise <mose.ns@hotmail.com>
2022-05-02 07:25:54 +0200

    tab functionality

--- frontend/src/components/home/services/ServiceTab.tsx
+++ frontend/src/components/home/services/ServiceTab.tsx
@@ -1,28 +1,46 @@
 import { ServiceProps } from "../../../types/Interface";
 import Button from "../../button/Button";
+import { useState } from "react";
 
 const Tab = ({ services }: ServiceProps) => {
+  const [value, setValue] = useState<number>(0);
+
   return (
     <>
       <div className="services__btns__container">
-        {services.map((service) => {
+        {services.map((service, index) => {
           const { id, title, icon } = service;
           return (
-            <Button className="services__btns" as="button" handleClick={() => console.log('ff')
-            } key={id}>
+            <Button
+              className={
+                index === value
+                  ? "services__btns activeTab__btn"
+                  : "services__btns"
+              }
+              as="button"
+              handleClick={() => setValue(index)}
+              key={id}
+            >
               <div className="btns__icons__container">{icon}</div>
               {title}
             </Button>
           );
         })}
       </div>
-      <div className="services__tab__content">
-        {services.map((service) => (
-          <p className="services__tab__P" key={service.id}>
+      <article className="services__tab__content">
+        {services.map((service, index) => (
+          <p
+            className={
+              index === value
+                ? "services__tab__P activeTab__text"
+                : "services__tab__P"
+            }
+            key={service.id}
+          >
             {service.text}
           </p>
         ))}
-      </div>
+      </article>
     </>
   );
 };
