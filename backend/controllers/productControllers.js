97dc4d5255af8d1039857e87d2ff6e4a37c639bf
moise <mose.ns@hotmail.com>
2022-05-05 01:49:50 +0200

    controllers productControllers

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,61 @@
+import Prduct from "../models/Product.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
+
+const createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json({ product });
+};
+
+const getAllProduct = async (req, res) => {
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products });
+};
+
+const getSingleProduct = async (req, res) => {
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+  res.status(StatusCodes.OK).json({ product });
+};
+
+const updateProduct = async (req, res) => {
+  const { id: productId } = req.params;
+  if (
+    name === "" ||
+    country === "" ||
+    price === "" ||
+    ref === "" ||
+    size === "" ||
+    bedroom === "" ||
+    bathroom === ""
+  )
+    throw new BadRequestError("You must provide all the values");
+  const products = await Product.findOneAndUpdate(
+    { _id: productId },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  if (!product)
+    throw new NotFoundError(`There is no product with id: ${productId}`);
+  res.status(StatusCodes.OK).json({ product });
+};
+
+const deleteProduct = async (req, res) => {
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+  if (!product)
+    throw new NotFoundError(`There is no product with id: ${productId}`);
+  await product.remove();
+  res.status(StatusCodes.OK).json({ product });
+};
+
+export {
+  createProduct,
+  getAllProduct,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+};
