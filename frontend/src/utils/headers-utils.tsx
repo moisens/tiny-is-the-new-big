80a636b5c8709fdd4000bc47239df012e5202438
moise <mose.ns@hotmail.com>
2022-05-11 03:29:16 +0200

    headers part 2

--- frontend/src/utils/headers-utils.tsx
+++ frontend/src/utils/headers-utils.tsx
@@ -6,9 +6,9 @@ export const HeaderBuy = [
   {
     id: "0a8dcd0a-d08b",
     image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1652197821/tiny-housing/tmp-1-1652197818014_rakj8t.jpg",
-    icon: <MdRoofing />,
+    icon: <MdRoofing size="1.5rem" />,
     category: "buy",
-    title: "Tiny houses quality, sustainable and ecological.",
+    title: "Quality, sustainable and ecological tiny houses.",
     description: "Our tiny houses are built by a team composed by architects, electricians, plumbers and of course professionals workers.We build for you and your love ones a safe tiny house, that is eco friendly.",
   }
 ];
@@ -16,7 +16,7 @@ export const HeaderRent = [
   {
     id: "0a8dcf4e-d08b",
     image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1652133266/tiny-housing/tmp-3-1652133265737_cutsmr.jpg",
-    icon: <MdOutlineBedroomParent />,
+    icon: <MdOutlineBedroomParent size="1.5rem" />,
     category: "rent",
     title: "Home is where you park. Especially when it is tiny and eco-friendly.",
     description: "Rent a tiny house built by our team of professionals. As said earlier, our tiny houses are sustainable and eco-friendly. You’ll get the comfort of a house without the inconvenience.",
