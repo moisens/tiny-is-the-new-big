41c1dd7a6033dc44870de2b076498b9831bae175
moise <mose.ns@hotmail.com>
2022-05-14 18:25:08 +0200

    Changed name files and componnent

--- frontend/src/pages/Buy.tsx
+++ frontend/src/pages/Buy.tsx
@@ -1,13 +1,13 @@
 import "../components/home/home.scss";
 import Headers from "../components/header/Headers";
-import PageComponent from "../components/pageComponent/PageComponent";
+import CardsPage from "../components/cardspage/Cardspage";
 import { HeaderBuy } from "../utils/headers-utils";
 
 const Buy = () => {
   return (
     <div className="home-container">
       <Headers dataHeaders={HeaderBuy} />
-      <PageComponent />
+      <CardsPage />
     </div>
   )
 }
