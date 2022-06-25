a435204a32f7e538d785901bb54a3bcccda626e4
moise <mose.ns@hotmail.com>
2022-06-25 23:23:59 +0200

    peoplesays component css grid part 6

--- frontend/src/components/home/peoplesays/PeopleSays.tsx
+++ frontend/src/components/home/peoplesays/PeopleSays.tsx
@@ -1,16 +1,13 @@
 import "./peoplesays.scss";
 import Slider from "./Slider";
-import { sliderPeople } from "../../../utils/utils"
-
+import { sliderPeople } from "../../../utils/utils";
 
 const PeopleSays = () => {
   return (
     <section className="people__container">
       <div className="people__triangle1"></div>
       <h2 className="people__titleH2">What people say about us.</h2>
-      <div className="people__slider">
-        <Slider sliderPeople={sliderPeople} />
-      </div>
+      <Slider sliderPeople={sliderPeople} />
       <div className="people__triangle2"></div>
     </section>
   );
--- frontend/src/components/home/peoplesays/Slider.tsx
+++ frontend/src/components/home/peoplesays/Slider.tsx
@@ -5,7 +5,7 @@ import {
 import { SliderPeople } from "../../../types/Interface";
 import IconsComponent from "../iconsComponent/IconsComponent";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 
 const Slider = ({ sliderPeople }: SliderPeople) => {
   const [index, setIndex] = useState<number>(0);
@@ -18,48 +18,84 @@ const Slider = ({ sliderPeople }: SliderPeople) => {
   }, [index, sliderPeople]);
 
   return (
-    <div className="peoplesSlider__container">
-      {/*Map from here*/}
-      {sliderPeople.map((slide, slideIndex) => {
-        const { id, name, text, image, icons } = slide;
-        console.log("SLIDE-INDEX: ", slideIndex);
-
-        let position: string = "nextSlide";
-        if (slideIndex === index) {
-          position = "activeSlide";
-        }
-
-        if (
-          slideIndex === index - 1 ||
-          (index === 0 && slideIndex === sliderPeople.length - 1)
-        ) {
-          position = "lastSlide";
-        }
-
-        return (
-          <div className={`peopleSlide__content ${position}`} key={id}>
-            <div className="peopleSlider__img">
-              <div className="people__avatar">
-                <img src={image} alt={name} title={name} />
-              </div>
-              <h4 className="people__person">{name}</h4>
-            </div>
-            <div className="people__stars">
-              <IconsComponent iconsItems={icons} />
-            </div>
-            <p className="people__saying">{text}</p>
+    <header className="peopleSlider__container">
+      <section className="peopleSlider__content">
+        {/*div to be returned*/}
+        <div className="sliderImages__container">
+          <div className="slider__image">
+            <img src="" alt="" title="" />
           </div>
-        );
-      })}
-      {/*end herE*/}
-      <div className="slider__previous">
-        <HiOutlineArrowNarrowLeft onClick={() => setIndex(index - 1)} />
-      </div>
-      <div className="slider__next">
-        <HiOutlineArrowNarrowRight onClick={() => setIndex(index + 1)} />
-      </div>
-    </div>
+          <div className="slider__stars">⭐️⭐️⭐️⭐️⭐️</div>
+          <div className="slider__text">
+            <p>
+              Broker ipsum dolor sit amet, consectetuer noted architect elit.
+              Modern interior commodo ligula eget dolor. Cum sociis panoramic
+              views penatibus et magnis dis downtown loft.
+            </p>
+          </div>
+        </div>
+        <div className="sliderImages__container">
+          <div className="slider__image">
+            <img src="" alt="" title="" />
+          </div>
+          <div className="slider__stars">
+            <h4 className="slider__title">name</h4>
+            ⭐️⭐️⭐️⭐️⭐️
+          </div>
+          <div className="slider__text">
+            <p>
+              Broker ipsum dolor sit amet, consectetuer noted architect elit.
+              Modern interior commodo ligula eget dolor. Cum sociis panoramic
+              views penatibus et magnis dis downtown loft.
+            </p>
+          </div>
+        </div>
+        {/*div to be returned*/}
+      </section>
+    </header>
   );
 };
 
 export default Slider;
+
+//gment>
+//  {/*Map from here*/}
+//  {sliderPeople.map((slide, slideIndex) => {
+//    const { id, name, text, image, icons } = slide;
+//    console.log("SLIDE-INDEX: ", slideIndex);
+//
+//    let position: string = "nextSlide";
+//    if (slideIndex === index) {
+//      position = "activeSlide";
+//    }
+//
+//    if (
+//      slideIndex === index - 1 ||
+//      (index === 0 && slideIndex === sliderPeople.length - 1)
+//    ) {
+//      position = "lastSlide";
+//    }
+//
+//    return (
+//      <div className={`peopleSlide__content ${position}`} key={id}>
+//        <div className="peopleSlider__img">
+//          <div className="people__avatar">
+//            <img src={image} alt={name} title={name} />
+//          </div>
+//          <h4 className="people__person">{name}</h4>
+//        </div>
+//        <div className="people__stars">
+//          <IconsComponent iconsItems={icons} />
+//        </div>
+//        <p className="people__saying">{text}</p>
+//      </div>
+//    );
+//  })}
+//  {/*end here*/}
+//  <div className="slider__previous">
+//    <HiOutlineArrowNarrowLeft onClick={() => setIndex(index - 1)} />
+//  </div>
+//  <div className="slider__next">
+//    <HiOutlineArrowNarrowRight onClick={() => setIndex(index + 1)} />
+//  </div>
+//</Fragment>
