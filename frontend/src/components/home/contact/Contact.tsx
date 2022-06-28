b87287da7e1810964158c532bfee07cf14f849b3
moise <mose.ns@hotmail.com>
2022-06-28 03:44:30 +0200

    contact css part 3

--- frontend/src/components/home/contact/Contact.tsx
+++ frontend/src/components/home/contact/Contact.tsx
@@ -1,12 +1,12 @@
 import "./contact.scss";
 import { MdOutlineContactPage } from "react-icons/md";
 import { MdOutlineCall } from "react-icons/md";
-import ContactIcon from "../../../assets/contact-icon.svg"
+import ContactIcon from "../../../assets/contact-icon.svg";
 
 import { countries } from "../../../utils/form-utils";
 import { services } from "../../../utils/form-utils";
 import { pohoneNumbers } from "../../../utils/form-utils";
-import SelectServices from "./Dropdown"
+import SelectServices from "./Dropdown";
 
 const Contact = () => {
   return (
@@ -15,9 +15,9 @@ const Contact = () => {
         <div className="contact__text">
           <div className="contact__illuContainer">
             <div className="contact__illu">
-            <img src={ContactIcon} alt="contact-icon" />
+              <img src={ContactIcon} alt="contact-icon" />
             </div>
-            
+
             <h4 className="contact__titleH4">
               Contact <span className="span__title">Us</span>
             </h4>
@@ -29,34 +29,54 @@ const Contact = () => {
           </p>
         </div>
         <div className="contact__images">
-          <div className="image__container">
-            {/* <img src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1650301425/tiny%20is%20the%20new%20big/tierra-mallorca-NpTbVOkkom8-unsplash_fcohsm.jpg" alt="" /> */}
-          </div>
-          <div className="image__container">
-            {/* <img src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1650300934/tiny%20is%20the%20new%20big/aysegul-yahsi-9QRqI6RbHWU-unsplash_m3dhn6.jpg" alt="" /> */}
-          </div>
-          <div className="image__container">
-            {/* <img src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1650301404/tiny%20is%20the%20new%20big/jet-dela-cruz-Gf6puSx3h6Y-unsplash_nfndgh.jpg" alt="" /> */}
-          </div>
+          <img
+            src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1650301425/tiny%20is%20the%20new%20big/tierra-mallorca-NpTbVOkkom8-unsplash_fcohsm.jpg"
+            alt=""
+          />
+          <img
+            src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1650300934/tiny%20is%20the%20new%20big/aysegul-yahsi-9QRqI6RbHWU-unsplash_m3dhn6.jpg"
+            alt=""
+          />
+          <img
+            src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1650301404/tiny%20is%20the%20new%20big/jet-dela-cruz-Gf6puSx3h6Y-unsplash_nfndgh.jpg"
+            alt=""
+          />
         </div>
       </div>
       <div className="form__container">
-        <form className ="form__content">
+        <form className="form__content">
           <div className="input__container">
             <label htmlFor="firstName" />
-            <input type="text" placeholder="First name" id="firstName" className="input__input"/>
+            <input
+              type="text"
+              placeholder="First name"
+              id="firstName"
+              className="input__input"
+            />
             <label htmlFor="lastName" />
-            <input type="text" placeholder="Last name" id="lastName" className="input__input"/>
+            <input
+              type="text"
+              placeholder="Last name"
+              id="lastName"
+              className="input__input"
+            />
           </div>
           <div className="input__container">
             <SelectServices dataDropdownList={countries} />
             <SelectServices dataDropdownList={services} />
           </div>
           <label htmlFor="email" />
-          <input type="text" id="email" placeholder="Your email" className="input__email" />
+          <input
+            type="text"
+            id="email"
+            placeholder="Your email"
+            className="input__email"
+          />
           <label htmlFor="subject" />
           <textarea id="subject" className="input__textarea" />
-          <button type="submit" className="form__button">Send</button>
+          <button type="submit" className="form__button">
+            Send
+          </button>
         </form>
         <div className="form__call">
           <button className="call__btn">
@@ -66,7 +86,6 @@ const Contact = () => {
           <div className="input__container">
             <SelectServices dataDropdownList={pohoneNumbers} />
           </div>
-          
         </div>
       </div>
       <div className="contact__triangle2"></div>
