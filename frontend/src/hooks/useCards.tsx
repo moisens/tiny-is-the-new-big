261f75462f7694a208e9046799bbf44f708ad523
moise <mose.ns@hotmail.com>
2022-09-08 17:23:36 +0200

    added context

--- frontend/src/hooks/useCards.tsx
+++ frontend/src/hooks/useCards.tsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
-import { CardsContext } from "../context/CardsContext";
+import { CardContext } from "../context/CardsContext";
 
 export const useCard = () => {
-  const context = useContext(CardsContext);
+  const context = useContext(CardContext);
   if (!context)
     throw new Error(`useCard must be used inside a CardsContext!!!`);
   return context;
