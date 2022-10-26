bea7c373a795b15f6aba30a6f1f3d3149547e39b
moise <mose.ns@hotmail.com>
2022-10-26 22:45:47 +0200

    fav func part 3

--- frontend/src/components/cardspage/CardList.tsx
+++ frontend/src/components/cardspage/CardList.tsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 import { BsHeart, BsHeartFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import { Productsdata } from "../../types/interface-housedata";
+import { CardDataType } from "../../types/interface-housedata";
 import houseConfiguration from "../../utils/configHouse";
 import useFavorite from "../../hooks/useFavorite";
 import Button from "../button/Button";
 
-const CardList = () => {
-  //console.log(product.price);
-  
-  
+const CardList = ({ product }: CardDataType) => {
+  const {
+    _id,
+    image,
+    country,
+    price,
+    category,
+    reference,
+    size,
+    bedroom,
+    bathroom,
+  } = product;
+
   const { addToFavorites, removeFromFavorites, favorites } = useFavorite();
   const [liked, setLiked] = useState(false);
 
@@ -17,164 +26,52 @@ const CardList = () => {
     setLiked(!liked);
   };
 
-  if (statusType === "pending") return <h2>Loading...</h2>;
-  if (statusType === "rejected") throw new Error(statusType.error); // need to be refactored!
-
   return (
-            <div className="page__card stacked" key={_id}>
-              <Link to={`/details-tiny-house/${_id}`}>
-                <img
-                  src={image[0]}
-                  alt={country}
-                  title={country}
-                  className="card__img"
-                />
-              </Link>
-              <div className="card__like">
-              {!liked && favorites ? (
-            <Button
-              as="button"
-              className=""
-              handleClick={() => {
-                handleActiveLike();
-                //addToFavorites(product);
-                console.log(favorites);
-              }}
-            >
-              <BsHeart />
-            </Button>
-          ) : (
-            <Button
-              as="button"
-              className=""
-              handleClick={() => {
-                handleActiveLike();
-                //removeFromFavorites(product);
-              }}
-            >
-              <BsHeartFill />
-            </Button>
-          )}
-              </div>
-              
-              <div className="card__content">
-                <div className="card__title">
-                  <h4 className="card__H4">{country}</h4>
-                </div>
-                <div className="card__title">
-                  <h4 className="card__H4">
-                    {/* <span className="green__span">
-                      {category === "rent"
-                        ? `€ ${price} / month`
-                        : `€ ${price}`}
-                    </span> */}
-                  </h4>
-                  <span className="green__span">|</span>
-                  <h4 className="card__H4">Ref: {reference}</h4>
-                </div>
-                <div className="card__title">
-                  {houseConfiguration(size, bedroom, bathroom)}
-                </div>
-              </div>
-            </div>
-          );
-  
-};
+    <div className="page__card stacked" key={_id}>
+      <Link to={`/details-tiny-house/${_id}`}>
+        <img
+          src={image[0]}
+          alt={country}
+          title={country}
+          className="card__img"
+        />
+      </Link>
+      <div className="card__like">
+        {
+          <Button
+          as="button"
+          className="like__icon"
+          handleClick={() => {
+            handleActiveLike();
+            addToFavorites(product);
+            console.log("Add",favorites);
+          }}
+        >
+          <BsHeart size="1.9rem" />
+        </Button>
+        
+        }
+      </div>
 
-export default CardList;
-
-
-
-/*
-
-const CardList = ({ productData, statusType }: Housedata) => {
-  const { products } = productData;
-  const { addToFavorites, removeFromFavorites, favorites } = useFavorite();
-  const [liked, setLiked] = useState(false);
-
-  const handleActiveLike = () => {
-    setLiked(!liked);
-  };
-
-  if (statusType === "pending") return <h2>Loading...</h2>;
-  if (statusType === "rejected") throw new Error(statusType); // need to be refactored!
-
-  return (
-    <React.Fragment>
-      {statusType === "resolved" &&
-        products?.map((product) => {
-          const {
-            _id,
-            image,
-            country,
-            price,
-            category,
-            size,
-            bedroom,
-            bathroom,
-            reference,
-          } = product;
-          return (
-            <div className="page__card stacked" key={_id}>
-              <Link to={`/details-tiny-house/${_id}`}>
-                <img
-                  src={image[0]}
-                  alt={country}
-                  title={country}
-                  className="card__img"
-                />
-              </Link>
-              <div className="card__like">
-              {!liked && favorites ? (
-            <Button
-              as="button"
-              className=""
-              handleClick={() => {
-                handleActiveLike();
-                addToFavorites(product);
-                console.log(favorites);
-              }}
-            >
-              <BsHeart />
-            </Button>
-          ) : (
-            <Button
-              as="button"
-              className=""
-              handleClick={() => {
-                handleActiveLike();
-                //removeFromFavorites(product);
-              }}
-            >
-              <BsHeartFill />
-            </Button>
-          )}
-              </div>
-              
-              <div className="card__content">
-                <div className="card__title">
-                  <h4 className="card__H4">{country}</h4>
-                </div>
-                <div className="card__title">
-                  <h4 className="card__H4">
-                    <span className="green__span">
-                      {category === "rent"
-                        ? `€ ${price} / month`
-                        : `€ ${price}`}
-                    </span>
-                  </h4>
-                  <span className="green__span">|</span>
-                  <h4 className="card__H4">Ref: {reference}</h4>
-                </div>
-                <div className="card__title">
-                  {houseConfiguration(size, bedroom, bathroom)}
-                </div>
-              </div>
-            </div>
-          );
-        })}
-    </React.Fragment>
+      <div className="card__content">
+        <div className="card__title">
+          <h4 className="card__H4">{country}</h4>
+        </div>
+        <div className="card__title">
+          <h4 className="card__H4">
+            <span className="green__span">
+              {category === "rent" ? `€ ${price} / month` : `€ ${price}`}
+            </span>
+          </h4>
+          <span className="green__span">|</span>
+          <h4 className="card__H4">Ref: {reference}</h4>
+        </div>
+        <div className="card__title">
+          {houseConfiguration(size, bedroom, bathroom)}
+        </div>
+      </div>
+    </div>
   );
 };
 
-*/
+export default CardList;
