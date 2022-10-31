cca4e1704955e73b6c1f7ea968257c8e1d83f030
moise <mose.ns@hotmail.com>
2022-10-31 21:51:00 +0100

    added mixin & variables to location section

--- frontend/src/components/home/location/Location.tsx
+++ frontend/src/components/home/location/Location.tsx
@@ -1,6 +1,5 @@
 import "./location.scss";
-import PinLocation from "../../../assets/location.svg";
-import FilledStar from "../../../assets/filled-star.svg";
+import { BsFillGeoAltFill } from "react-icons/bs";
 import World from "../../../assets/world.png";
 import Button from "../../../components/button/Button"
 import { LocationDataType } from "../../../types/interface-Locationsdata";
@@ -60,7 +59,7 @@ useEffect(() => {
         <div className="location__text">
           <div className="illu__container">
             <div className="location__illu">
-              <img src={PinLocation} alt="location" />
+              <BsFillGeoAltFill size="1.8rem" color="#8db530" />
             </div>
             <h4 className="title__H4">
               Our <span className="span__title">Locations</span>
@@ -81,9 +80,6 @@ useEffect(() => {
         <div className="location__slider">
           <div className="slider__container">
             <LocationSlider locationDatas={datas} statusType={status} />
-            
-            
-            
           </div>
         </div>
       </div>
@@ -95,55 +91,3 @@ useEffect(() => {
 };
 
 export default Location;
-
-
-
-/*
-
-<div className="slider__content">
-<div className="slider__imgText">
-  <div className="slider__img">
-    <img
-      src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1650073308/tiny%20is%20the%20new%20big/andy-holmes-XZ8AiqUcbD8-unsplash_aqtwg8.jpg"
-      alt="location"
-      title="location_name"
-    />
-  </div>
-  <div className="slider__text">
-    <h3 className="title__H3">British Columbia Canada</h3>
-    <p className="slider__description">
-      magnis dis downtown loft montes, nascetur soaring views mus.
-    </p>
-    <div className="review__container">
-      <div className="review__images">
-        <div className="review__img">
-          <img
-            src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1649899897/tiny%20is%20the%20new%20big/christopher-campbell-rDEOVtE7vOs-unsplash_vnbxj5.jpg"
-            alt="avatar"
-          />
-        </div>
-        <div className="review__img">
-          <img
-            src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1649902871/tiny%20is%20the%20new%20big/jake-nackos-IF9TK5Uy-KI-unsplash_cjx1sp.jpg"
-            alt="avatar"
-          />
-        </div>
-        <div className="review__img">
-          <img
-            src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1650073476/tiny%20is%20the%20new%20big/kal-visuals-t_z2UegYC-g-unsplash_hn7glw.jpg"
-            alt="avatar"
-          />
-        </div>
-      </div>
-      <div className="review__starText">
-        <div className="review__star">
-          <img src={FilledStar} alt="star" />
-        </div>
-        <p className="review__P">5 (10 reviews)</p>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-
-*/
