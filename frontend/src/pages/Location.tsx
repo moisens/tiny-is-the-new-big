67965ed131f68353dac63c310d88ef51a89054b2
moise <mose.ns@hotmail.com>
2023-01-05 00:16:01 +0100

    Location page refactoring part 1

--- frontend/src/pages/Location.tsx
+++ frontend/src/pages/Location.tsx
@@ -5,49 +5,16 @@ import LocationPage from "../components/cardspage/LocationPage";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorBoundaryFallBack from "../components/errorBounderies/Errorboundaries";
 
-interface IsFetchingError {
-  message: string;
-}
 
-const isError = (error: unknown): error is IsFetchingError => {
-  if (error && typeof error === "object" && "message" in error) {
-    return true;
-  }
-  return false;
-};
 
 const Location = () => {
-  const [datas, setDatas] = useState<LocationDataType>({} as LocationDataType);
-  const [status, setStatus] = useState<StatusType>("iddle");
-  const [error, setError] = useState<unknown>(null);
 
-  const fetchHouses = async () => {
-    try {
-      setStatus("pending");
-      const response = await fetch(`/api/v1/locations`);
-      if (!response.ok) {
-        throw new Error(`unable to fetch data!`);
-      }
-      const jsonhouse = await response.json();
-      setStatus("resolved");
-      setDatas(jsonhouse);
-    } catch (error) {
-      if (isError(error)) {
-        setError(error.message);
-        setStatus("rejected");
-        console.log(error);
-      }
-    }
-  };
-
-  useEffect(() => {
-    fetchHouses();
-  }, []);
+ 
 
   return (
     <div className="home-container">
       <ErrorBoundary FallbackComponent={ErrorBoundaryFallBack}>
-        <LocationPage locationDatas={datas} statusType={status} />
+        <LocationPage />
       </ErrorBoundary>
     </div>
   );
