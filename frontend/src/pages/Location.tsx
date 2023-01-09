7ccc884c7b9c87167d85f31ce0a73abe7bb1942a
moise <mose.ns@hotmail.com>
2023-01-09 17:16:40 +0100

    fetching locations data + locationList component

--- frontend/src/pages/Location.tsx
+++ frontend/src/pages/Location.tsx
@@ -1,20 +1,16 @@
-import { useState, useEffect } from "react";
 import { LocationDataType } from "../types/interface-Locationsdata";
-import { StatusType } from "../types/interface-housedata";
 import LocationPage from "../components/cardspage/LocationPage";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorBoundaryFallBack from "../components/errorBounderies/Errorboundaries";
-
-
+import useFetch from "../hooks/useFetch";
 
 const Location = () => {
-
- 
+  const { dataHouse: locations, status, error } = useFetch<LocationDataType>("/api/v1/locations");
 
   return (
     <div className="home-container">
       <ErrorBoundary FallbackComponent={ErrorBoundaryFallBack}>
-        <LocationPage />
+        <LocationPage locationDatas={locations} status={status} error={error} />
       </ErrorBoundary>
     </div>
   );
--- frontend/src/types/interface-Locationsdata.ts
+++ frontend/src/types/interface-Locationsdata.ts
@@ -2,13 +2,18 @@ import { StatusType } from "./interface-housedata";
 
 export interface Locations {
   locationDatas: LocationDataType;
-  statusType: StatusType;
+  status: string;
+  error: unknown;
 }
 
 export type LocationDataType = {
   locations: ProductLocationsdata[];
   totalLocations: string;
 };
+
+export type CardDataLocationType = {
+  location: ProductLocationsdata
+}
 
 export type ProductLocationsdata = {
   readonly _id: string;
