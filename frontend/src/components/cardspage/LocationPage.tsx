5a3376f1c457019e127b16cbe008095c3fbb5913
moise <mose.ns@hotmail.com>
2023-01-14 02:34:04 +0100

    map active state

--- frontend/src/components/cardspage/LocationPage.tsx
+++ frontend/src/components/cardspage/LocationPage.tsx
@@ -1,8 +1,10 @@
 import "./locations.scss";
 import { Locations } from "../../types/interface-Locationsdata";
 import LocationsList from "./LocationsList";
+import { useState } from "react";
 
 const LocationPage = ({ locationDatas, status, error }: Locations) => {
+  const [value, setValue] = useState(0);
   const { locations } = locationDatas;
 
   if (status === "pending") return <h2>Loading...</h2>;
@@ -12,19 +14,31 @@ const LocationPage = ({ locationDatas, status, error }: Locations) => {
     <section className="loc__container">
       <section className="loc__content">
         {status === "resolved" &&
-          locations?.map((location) => {
+          locations?.map((location, index) => {
             const { _id } = location;
-            return <LocationsList location={location} key={_id} />;
+            return (
+              <LocationsList
+                location={location}
+                index={index}
+                value={value}
+                setValue={setValue}
+                key={_id}
+              />
+            );
           })}
       </section>
       <section className="loc__map">
         <div className="mapouter">
           <div className="gmap_canvas">
-            {locations?.map((location) => {
+            {locations?.map((location, index) => {
               const { googolLink, country } = location;
               return (
                 <iframe
-                  className="iframe__container"
+                  className={
+                    index === value
+                      ? "iframe__container activemap"
+                      : "iframe__container"
+                  }
                   width="700"
                   height="650"
                   id="gmap_canvas"
