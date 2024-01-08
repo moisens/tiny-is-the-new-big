cb90a096287c75cd4006975f2fc2b24adb905df9
Moise <moise_ns@outlook.com>
2024-01-08 23:12:06 +0100

    deleted package lock + modif vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000",
+        target: "http://127.0.0.1:6000",
         changeOrigin: true,
         secure: false,
       },
@@ -17,5 +17,5 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1600,
   },
-  plugins: [react()]
-})
+  plugins: [react()],
+});
