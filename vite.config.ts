f829a183ced06b013e3fab80434a1fe2c5b88bcd
Moise <moise_ns@outlook.com>
2024-03-17 18:39:20 +0100

    modifying endpoints

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     host: true,
-    port: 3000,
+    port: 8080,
     proxy: {
       "/api": {
         target: "https://tinyhousing-backend-ezprh.ondigitalocean.app",
